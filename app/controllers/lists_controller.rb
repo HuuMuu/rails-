b7c536604994dcc4cb1485a9bdaeddf9bbcbcce3
EC2 Default User <ec2-user@ip-172-31-75-144.ec2.internal>
2022-07-04 15:28:59 +0000

    rails ２章　投稿機能を作ろう end

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,18 @@
 class ListsController < ApplicationController
   def new
+    #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
   end
+
+
+def create
+  # 1.&2. データを受け取り新規登録するためのインスタンスを作成
+  list = List.new(list_parames)
+  #3.データをデータベースに保存するためのsaveメソッド実行
+  list.save
+  #4,トップ画面へリダイレクト
+  redirect_to '/top'
+end
 
   def index
   end
@@ -9,5 +21,12 @@ class ListsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  #ストロングパラメータ
+  def list_parames
+    params.require(:list).permit(:title, :body)
   end
 end
