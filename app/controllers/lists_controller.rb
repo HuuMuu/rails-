736370a15b35b3f9c2d0e6495922825aa8c58b40
EC2 Default User <ec2-user@ip-172-31-75-144.ec2.internal>
2022-07-08 13:24:38 +0000

    rails 第9章 redirect_toとrender の処理の違いについて end

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,11 +7,14 @@ class ListsController < ApplicationController
 
 def create
   # 1.&2. データを受け取り新規登録するためのインスタンスを作成
-  list = List.new(list_parames)
+  @list = List.new(list_parames)
   #3.データをデータベースに保存するためのsaveメソッド実行
-  list.save
+  if @list.save
   #4,トップ画面へリダイレクト
-  redirect_to list_path(list.id)
+    redirect_to list_path(list.id)
+  else
+    render :new
+  end
 end
 
   def index
@@ -30,6 +33,12 @@ end
     list = List.find(params[:id])
     list.update(list_parames)
     redirect_to list_path(list. id)
+  end
+  
+  def destroy
+    list = List.find(params[:id]) #データ（レコード）を1件獲得
+    list.destroy #データ（レコード）を削除
+    redirect_to '/lists' #投稿一覧画面へリダイレクト
   end
 
   private
